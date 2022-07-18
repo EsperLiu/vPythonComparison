f4cf4fcffce0f0fd610c7f4ec1aa9211ca990f95
Tao Liu <n9912517@qut.edu.au>
2022-07-18 12:30:12 +1000

    version ready

---
+++ addition/addition.py
@@ -0,0 +1,10 @@
+def start():
+	return
+def end():
+	return
+	
+start()
+a = 1
+b = 2
+print(a + b)
+end()
---
+++ hello/hello.py
@@ -0,0 +1,8 @@
+def start():
+	return
+def end():
+	return
+	
+start()
+print("Hello World")
+end()
